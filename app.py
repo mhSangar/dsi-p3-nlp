5f0690fc52e0b21199f2b37882fca0e551491b68
Mario Sánchez García <mario.sangar@hotmail.com>
2019-04-07 20:19:24 +0200

    use a diff n from length of sentence

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ logger = logging.getLogger(__name__)
 def initLogger(level='INFO'):
     coloredlogs.install(fmt='%(asctime)s - %(levelname)s: %(message)s',
                         datefmt='%d/%m/%Y %H:%M:%S', level=level)
-    logger.info('Logger is active')
+    logger.debug('Logger is active')
 
 
 def downloadNltkDependencies():
@@ -36,10 +36,21 @@ def downloadNltkDependencies():
         download('stopwords')
 
 
-def openCorpus(filename):
-    with open(filename, 'r') as f:
-        str = f.read()
-        return str
+def openCorpus(filename, test=False):
+    if not test:
+        with open(filename, 'r') as f:
+                str = f.read()
+                return str
+    else:
+        return '''
+            And he went back to meet the fox. "Goodbye," he said.
+
+            "Goodbye," said the fox. "And now here is my secret, a very simple secret: It is only with the
+            heart that one can see rightly; what is essential is invisible to the eye."
+
+            "What is essential is invisible to the eye," the little prince repeated, so that he would be sure to
+            remember. 
+        '''
 
 
 def createNGrams(tokens, nbrOfNGrams):
@@ -58,6 +69,7 @@ def probabilityPerGram(tokens, n, order=False):
     fdist = None
     fdistMinus1 = None
 
+    unigram = createNGrams(tokens, 1)
     ngrams = createNGrams(tokens, n)
     ngramsMinus1 = createNGrams(tokens, n-1)
 
@@ -68,7 +80,7 @@ def probabilityPerGram(tokens, n, order=False):
         # print(gram)
         # print(value)
         # print(fdistMinus1[gram[:-1]])
-        fdist[gram] = (value / fdistMinus1[gram[:-1]]) * 100
+        fdist[gram] = (value / fdistMinus1[gram[:-1]]) #* 100
 
     # list with the results of the frequency
     gramList = []
@@ -94,45 +106,62 @@ def removeStopWords(wordList, language='english'):
     return filteredWordList
 
 
-def probabilityOfSentence(sentence, corpusPath, cleanStopWords=False):
+def probabilityOfSentence(sentence, nbrOfNGrams, corpusPath, toLowerCase=True, cleanStopWords=False):
     corpus = openCorpus(corpusPath)
 
     tokenizer = RegexpTokenizer(r'\w+')
-    
     sentenceTokens = tokenizer.tokenize(sentence)
-    # length of sentence
-    n = len(sentenceTokens)
-
     corpusTokens = tokenizer.tokenize(corpus)
 
     # clean stop words
     if cleanStopWords:
         corpusTokens = removeStopWords(corpusTokens)
+        sentenceTokens = removeStopWords(sentenceTokens)
 
     # we pass the words to lowercase to avoid double entries
-    for i in range(0, len(corpusTokens)):
-        corpusTokens[i] = corpusTokens[i].lower()
+    if toLowerCase:
+        for i in range(0, len(corpusTokens)):
+            corpusTokens[i] = corpusTokens[i].lower()
+        for i in range(0, len(sentenceTokens)):
+            sentenceTokens[i] = sentenceTokens[i].lower()
 
-    gramList = probabilityPerGram(corpusTokens, n)
+    gramList = probabilityPerGram(corpusTokens, nbrOfNGrams)
     
-    for item in gramList:
-        if item['gram'] == tuple(sentenceTokens):
-            return item['value']
+    sentenceTuples = []
+    for i in range(0, len(sentenceTokens)):
+        tup = sentenceTokens[i:i+nbrOfNGrams]
+        if len(tup) == nbrOfNGrams:
+            sentenceTuples.append(tup)
+
+    prob = -1
+    for tup in sentenceTuples:
+        found = False
+        for item in gramList:
+            if item['gram'] == tuple(tup):
+                logger.debug('{ngram}: {prob:.5f} %'.format(ngram=tup, prob=item['value'] * 100))
+                prob *= item['value']
+                found = True
+        
+        if not found:
+            logger.debug('{ngram}: {prob:.5f} %'.format(ngram=tup, prob=0 * 100))
+            prob = 0
     
-    return 0
+    return -prob
 
 
 def main():
-    sentence = sys.argv[1]
-    
-    p = probabilityOfSentence(sentence, 'corpus/dracula.txt')
+    nbrOfNGrams = int(sys.argv[1])
+    logger.info('NbrOfNGrams: {n}'.format(n=nbrOfNGrams))
+    sentence = sys.argv[2]
+    logger.info('Sentence:    "{s}"'.format(s=sentence))
 
-    logger.info('sentence: "{}"'.format(sentence))
-    logger.info('prob: {:.2f}%'.format(p))
+    p = abs(probabilityOfSentence(sentence, nbrOfNGrams, 'corpus/saint-exupery-little-prince.txt'))
+
+    logger.info('prob: {:.2f}%'.format(p*100))
 
 
 if __name__ == '__main__':
-    initLogger()
+    initLogger('DEBUG')
     downloadNltkDependencies()
 
     try:
